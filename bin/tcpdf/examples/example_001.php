40da21c9f3b66e1f67781e922897c28509b0a639
adwisatya <a.dwisaty4@yahoo.com>
2015-04-06 03:10:27 +0700

    tcpdf able to print struct ex_001

--- bin/tcpdf/examples/example_001.php
+++ bin/tcpdf/examples/example_001.php
@@ -31,14 +31,14 @@ require_once('tcpdf_include.php');
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
 // set document information
-$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetCreator('Administrator CV Delta');
 $pdf->SetAuthor('CV Delta');
 $pdf->SetTitle('Struk Service');
 $pdf->SetSubject('Pelanggan Nomor XXX');
 $pdf->SetKeywords('Struk');
 
 // set default header data
-$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 001', PDF_HEADER_STRING, array(0,64,255), array(0,64,128));
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, "CV. DELTA Electronic Repair Center", "Jalan Terusan Buah Batu 10, 40123, 085721169983", array(0,64,255), array(0,64,128));
 $pdf->setFooterData(array(0,64,0), array(0,64,128));
 
 // set header and footer fonts
@@ -85,11 +85,31 @@ $pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'colo
 
 // Set some content to print
 $html = <<<EOD
-<h1>Welcome to <a href="http://www.tcpdf.org" style="text-decoration:none;background-color:#CC0000;color:black;">&nbsp;<span style="color:black;">TC</span><span style="color:white;">PDF</span>&nbsp;</a>!</h1>
-<i>This is the first example of TCPDF library.</i>
-<p>This text is printed using the <i>writeHTMLCell()</i> method but you can also use: <i>Multicell(), writeHTML(), Write(), Cell() and Text()</i>.</p>
-<p>Please check the source code documentation and other examples for further information.</p>
-<p style="color:#CC0000;">TO IMPROVE AND EXPAND TCPDF I NEED YOUR SUPPORT, PLEASE <a href="http://sourceforge.net/donate/index.php?group_id=128076">MAKE A DONATION!</a></p>
+<h3>Nota Nomor:</h3>
+<br/>
+<table>
+	<tr>
+		<td>No</td>
+		<td>Barang</td>
+		<td>Jumlah</td>
+		<td>Harga Satuan</td>
+		<td>Total</td>
+	</tr>
+	<tr>
+		<td>1</td>
+		<td>Arduino</td>
+		<td>2</td>
+		<td>300000</td>
+		<td>600000</td>
+	</tr>
+	<tr>
+		<td>2</td>
+		<td>Rasberry</td>
+		<td>1</td>
+		<td>250000</td>
+		<td>250000</td>
+	</tr>
+</table>
 EOD;
 
 // Print text using writeHTMLCell()
@@ -99,7 +119,7 @@ $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
 
 // Close and output PDF document
 // This method has several options, check the source code documentation for more information.
-$pdf->Output('example_001.pdf', 'F');
+$pdf->Output(__DIR__ .'/../../../files/struk/'.'example_001.pdf', 'F');
 
 //============================================================+
 // END OF FILE
