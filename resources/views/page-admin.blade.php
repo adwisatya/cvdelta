a0c5c6faf53860ab101300f75617a9cea8d6db04
Rikysamuel <rikz.samuel@gmail.com>
2015-04-13 18:34:24 +0700

    add yield

--- resources/views/page-admin.blade.php
+++ resources/views/page-admin.blade.php
@@ -22,7 +22,7 @@
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Component<b class="caret"></b></a>
 						<ul class="dropdown-menu">
-							<li><a href="#">Requested Component</a></li>
+							<li><a href="{{ url('/parkir') }}">Requested Component</a></li>
 							<li><a href="#">stocks</a></li>
 							<li><a href="#">stocks run out</a></li>
 							<li><a href="#">add stock</a></li>
@@ -51,6 +51,7 @@
 	</div> 
 	<div class="container">
 		
+	@yield('content')
 
 	</div>
 	<footer class="footer">
