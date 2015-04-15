39961058c3a35c61d8e6e1c5a3c005ad1763e6fe
Rikysamuel <rikz.samuel@gmail.com>
2015-04-16 00:36:20 +0700

    add stock component

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,3 +29,4 @@ Route::get('admin/request', 'ComponentController@request');
 Route::get('admin/stock', 'ComponentController@stock');
 Route::get('admin/minimum', 'ComponentController@min');
 Route::get('admin/add', 'ComponentController@add');
+Route::post('admin', 'ComponentController@input');
