88d538a053b929c1682f67bd36700d06fb36080e
adwisatya <a.dwisaty4@yahoo.com>
2015-04-15 22:10:06 +0700

    setelah revisi

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,7 +32,4 @@ Route::get('invoice','SiteController@invoice');
 Route::get('admin/request', 'ComponentController@request');
 Route::get('admin/stock', 'ComponentController@stock');
 Route::get('admin/minimum', 'ComponentController@min');
-<<<<<<< HEAD
-=======
 Route::get('admin/add', 'ComponentController@add');
->>>>>>> 4a3ecf7c6cca76acb3509d8e19407bce5bff897a
