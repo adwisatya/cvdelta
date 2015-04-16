a1dae57ed1bb9e758489656ff2a356f1ad6311d0
Chrestella <chres_tella08@yahoo.com>
2015-04-16 21:35:04 +0700

    login nav

--- resources/views/page-admin.blade.php
+++ resources/views/page-admin.blade.php
@@ -15,7 +15,7 @@
 			<div class="navbar-header">
 				<!-- Button for smallest screens -->
 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-				<a class="navbar-brand" href="#"><img src="{{ asset('/images/logo.png') }}" alt="CV. Delta logo"></a>
+				<a class="navbar-brand" href="{{ url('/index') }}"><img src="{{ asset('/images/logo.png') }}" alt="CV. Delta logo"></a>
 			</div>
 			<div class="navbar-collapse collapse">
 				<ul class="nav navbar-nav pull-right">
