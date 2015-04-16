251565839c05ac2160ce47deb63756a47ea82a4b
Chrestella <chres_tella08@yahoo.com>
2015-04-16 17:14:25 +0700

    seeder

--- database/seeds/TagihanSeeder.php
+++ database/seeds/TagihanSeeder.php
@@ -19,7 +19,7 @@ class TagihanSeeder extends Seeder {
         ] );
 
         Tagihan::create( [
-            'no_seri_komponen' => 'k010203-1' ,
+            'no_seri_komponen' => 'k010203-2' ,
             'no_seri_barang_rusak' => '12345-1' ,
             'tgl' => '2015-04-13' ,
             'no_tagihan' => '234',
@@ -27,7 +27,7 @@ class TagihanSeeder extends Seeder {
         ] );
 
         Tagihan::create( [
-            'no_seri_komponen' => 'k010203-1' ,
+            'no_seri_komponen' => 'k010203-3' ,
             'no_seri_barang_rusak' => '12345-2' ,
             'tgl' => '2015-04-13' ,
             'no_tagihan' => '234',
@@ -35,7 +35,7 @@ class TagihanSeeder extends Seeder {
         ] );
 
         Tagihan::create( [
-            'no_seri_komponen' => 'k010203-1' ,
+            'no_seri_komponen' => 'k010203-4' ,
             'no_seri_barang_rusak' => '12345-3' ,
             'tgl' => '2015-04-13' ,
             'no_tagihan' => '234',
@@ -43,12 +43,43 @@ class TagihanSeeder extends Seeder {
         ] );
 
         Tagihan::create( [
-            'no_seri_komponen' => 'k010203-1' ,
+            'no_seri_komponen' => 'k010203-5' ,
             'no_seri_barang_rusak' => '12345-4' ,
             'tgl' => '2015-04-13' ,
             'no_tagihan' => '234',
             'status' => 'billed',
         ] );
+
+        Tagihan::create( [
+            'no_seri_komponen' => 'k010203-5' ,
+            'no_seri_barang_rusak' => '12345-6' ,
+            'tgl' => '2015-04-13' ,
+            'no_tagihan' => '234',
+            'status' => 'billed',
+        ] );
         
+        Tagihan::create( [
+            'no_seri_komponen' => 'k010203-5' ,
+            'no_seri_barang_rusak' => '12345-7' ,
+            'tgl' => '2015-04-13' ,
+            'no_tagihan' => '234',
+            'status' => 'billed',
+        ] );
+
+        Tagihan::create( [
+            'no_seri_komponen' => 'k010203-5' ,
+            'no_seri_barang_rusak' => '12345-7' ,
+            'tgl' => '2015-04-13' ,
+            'no_tagihan' => '234',
+            'status' => 'billed',
+        ] );
+
+        Tagihan::create( [
+            'no_seri_komponen' => 'k010203-5' ,
+            'no_seri_barang_rusak' => '12345-7' ,
+            'tgl' => '2015-04-13' ,
+            'no_tagihan' => '234',
+            'status' => 'billed',
+        ] );
     }
 }
