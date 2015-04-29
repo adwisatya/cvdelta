a957ef761d9dc65578a0df6e812efde6a1848ea1
adwisatya <a.dwisaty4@yahoo.com>
2015-04-29 09:40:42 +0700

    Revisi Route

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -53,7 +53,7 @@ class LoginController extends Controller {
 			if($userTeknisi->password == $inputPassword){
 				Session::put('username', $inputUsername);
 				Session::put('role', 'teknisi');
-				return redirect('/index');
+				return redirect('/');
 			}
 		}elseif($userAdministrasi != NULL){
 			if($userAdministrasi->password == $inputPassword){
