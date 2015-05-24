4019e3b3ead1439e3cad1b179654954c1d50cf4f
Chrestella <chres_tella08@yahoo.com>
2015-05-24 13:42:55 +0700

    barang rusak -admin

--- resources/views/barang-rusak.blade.php
+++ resources/views/barang-rusak.blade.php
@@ -39,7 +39,6 @@
 					<th>Customer</th>
 					<th>No Surat Jalan</th>
 					<th>Tgl Datang</th> 
-					<th>Biaya Jasa</th> 
 					<th>Status</th> 
 					<th>Tgl Diperbaiki</th> 
 					<th>Teknisi</th> 
@@ -55,11 +54,11 @@
 		            <td>{{$barang->nama_perusahaan}}</td>
 		            <td>{{$barang->no_surat_jalan}}</td>
 		            <td>{{$barang->tgl_datang}}</td>
-		            <td><input type="text" class="form-control" name="hargaJasa" form="form_selesai"></td>
 		            <td>{{$barang->status}}</td>
 		            <td>{{$barang->tgl_diperbaiki}}</td>
 		           	<td id="{{$barang->no_seri_barang_rusak}}">@if($barang->username=="")
-		            		<a href="#perbaiki"><button class="btn btn-primary pilihTeknisi" role="button"> Pilih</button></a>
+		           			-
+		            		<!-- <a href="#perbaiki"><button class="btn btn-primary pilihTeknisi" role="button"> Pilih</button></a> -->
 		            	@else
 		            		{{$barang->username}}
 		            	@endif		            	
