d0d1a1d9293461740518b5d2ab0f85d66fc4c1db
Rikysamuel <rikz.samuel@gmail.com>
2015-08-27 22:03:12 +0700

    add nomor tagihan

--- app/Tagihan.php
+++ app/Tagihan.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
  
 class Tagihan extends Model {
     protected $table = 'tagihan';
-    protected $primaryKey = 'no_tagihan';
     public $timestamps = false;
 
 }
